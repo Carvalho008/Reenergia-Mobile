b394709af388c8070b9cbcf86ec16b614a313dd6
Lian Carvalho <carvalhocosta842@gmail.com>
2025-06-28 12:23:37 -0300

    Transforma a tipografia em estilo

--- reenergia/constants/Typografy.ts
+++ reenergia/constants/Typografy.ts
@@ -1,9 +1,11 @@
-export const fonts = {
-    xl: 26,
-    l: 22,
-    m: 18,
-    s: 16,
-    xs: 15,
-    bold: 'bold',
-}
+import { StyleSheet } from "react-native";
+
+export const fonts = StyleSheet.create({
+    xl: { fontSize: 26 },
+    l: { fontSize: 22 },
+    m: { fontSize: 18 },
+    s: { fontSize: 16 },
+    xs: { fontSize: 15 },
+    bold: { fontWeight: 'bold' },
+})
 
