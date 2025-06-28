2bfac20dd37f830a0eaaef5c58586e2899c8c588
Lian Carvalho <carvalhocosta842@gmail.com>
2025-06-28 12:46:49 -0300

    Página Home - Modificações iniciais

--- reenergia/app/(tabs)/index.tsx
+++ reenergia/app/(tabs)/index.tsx
@@ -1,24 +1,24 @@
-import { colors } from '@/constants/Colors';
-
 import { View, Text, StyleSheet } from 'react-native';
+
+import { colors } from '@/constants/Colors';
+import { globalStyles } from '@/constants/Styles';
+import { fonts } from '@/constants/Typografy';
+
+import Header from '../components/Header';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bem-vindo ao app!</Text>
+    <View style={globalStyles.screenMain}>
+      <Header icon='home' title='Home' />
+      <View style={globalStyles.sectionBlock}>
+        <Text style={[fonts.l, fonts.bold, { color: colors.green2 }]}>
+          Descubra qual <Text style={{ color: colors.green1 }}>Energia Renovável</Text> é ideal para você
+        </Text>
+        <Text style={[fonts.s, { color: colors.gray }]}>Compare fontes, entenda os impactos e
+          tome decisões conscientes.</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.shadowGreen,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+const styles = StyleSheet.create({});
