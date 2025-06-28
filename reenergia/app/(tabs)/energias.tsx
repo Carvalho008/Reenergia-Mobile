d04f3ba49f3caa1c32217ecb6ed6ea133e7fab82
Lian Carvalho <carvalhocosta842@gmail.com>
2025-06-28 13:16:43 -0300

    Página Energias - Modificações iniciais

--- reenergia/app/(tabs)/energias.tsx
+++ reenergia/app/(tabs)/energias.tsx
@@ -1,24 +1,23 @@
-import { colors } from '@/constants/Colors';
-
 import { View, Text, StyleSheet } from 'react-native';
+
+import { colors } from '@/constants/Colors';
+import { globalStyles } from '@/constants/Styles';
+import { fonts } from '@/constants/Typografy';
+
+import Header from '../components/Header';
 
 export default function EnergiasScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tela de energias</Text>
+    <View style={globalStyles.screenMain}>
+      <Header icon='earth' title='Energias pelo mundo' />
+      <View style={globalStyles.sectionBlock}>
+        <Text style={[fonts.m, fonts.bold, { color: colors.green2 }]}>Destaque</Text>
+      </View>
+      <View style={globalStyles.sectionBlock}>
+        <Text style={[fonts.m, fonts.bold, { color: colors.green1 }]}>Gráficos</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.shadowGreen,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+const styles = StyleSheet.create({});
