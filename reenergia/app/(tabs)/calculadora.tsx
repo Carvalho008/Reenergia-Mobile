a2badf59d1ac7507113820bb17250ed252033ec7
Lian Carvalho <carvalhocosta842@gmail.com>
2025-08-23 10:15:45 -0300

    Tela Calculadora com switch

--- reenergia/app/(tabs)/calculadora.tsx
+++ reenergia/app/(tabs)/calculadora.tsx
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 import { colors } from '@/constants/Colors';
 import { globalStyles } from '@/constants/Styles';
@@ -7,11 +8,72 @@ import { fonts } from '@/constants/Typografy';
 import Header from '../components/Header';
 
 export default function CalculadoraScreen() {
+
+  const [calc, setCalc] = useState(true)
+
   return (
     <View style={globalStyles.screenMain}>
       <Header icon='calculator' title='Calculadora' />
+
+      <View style={styles.switch}>
+        {/* Botão Calculadora */}
+        <Pressable style={[styles.switchButton, calc && styles.active,
+        { borderTopLeftRadius: 50, borderBottomLeftRadius: 50 },]}
+          onPress={() => setCalc(true)}>
+          <Text style={[{ color: colors.gray1 }, calc && styles.active]}>
+            Calculadora
+          </Text>
+        </Pressable>
+
+        {/* Botão Resultados */}
+        <Pressable style={[styles.switchButton, !calc && styles.active,
+        { borderTopRightRadius: 50, borderBottomRightRadius: 50 },]}
+          onPress={() => setCalc(false)}>
+          <Text style={[{ color: colors.gray1 }, !calc && styles.active]}>
+            Resultados
+          </Text>
+        </Pressable>
+      </View>
+
+      {/* Seção Calculadora */}
+      {calc && (
+        <View style={globalStyles.sectionBlock}>
+          <Text style={[fonts.s, fonts.bold, { color: colors.green2 }]}>Encontre a melhor energia para você...</Text>
+        </View>
+      )}
+
+
+      {/* Seção Resultados */}
+      {!calc && (
+        <View style={[globalStyles.sectionBlock, { backgroundColor: colors.white }]}>
+          <Text style={[fonts.s, fonts.bold, { color: colors.black }]}>Resultados</Text>
+        </View>
+      )}
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  switch: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 1,
+  },
+  switchButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    gap: 10,
+    padding: 8,
+    backgroundColor: colors.grayMinus1,
+    opacity: 0.7,
+    cursor: 'pointer',
+  },
+  active: {
+    backgroundColor: colors.greenMinus2,
+    color: colors.green1,
+    opacity: 1,
+  }
+});
