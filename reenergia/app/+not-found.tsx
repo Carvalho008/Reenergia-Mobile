abb91a87511abfe59341614a8beb76bb50584bea
Lian Carvalho <carvalhocosta842@gmail.com>
2025-08-29 14:36:25 -0300

    Tela 404 melhorada

--- reenergia/app/+not-found.tsx
+++ reenergia/app/+not-found.tsx
@@ -1,32 +1,33 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import {Link, Stack} from 'expo-router'
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { colors } from '@/constants/Colors';
+import { globalStyles } from '@/constants/Styles';
+import { fonts } from '@/constants/Typografy';
+
+import Header from './components/Header';
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
+    <View style={globalStyles.screenMain}>
+      <Stack.Screen options={{headerShown: false}}/>
+
+      <Header icon='close' title='Não encontrado'/>
+
+      <View style={globalStyles.sectionBlock}>
+        <Text style={[fonts.l, fonts.bold, { color: colors.green2 }]}>
+          Erro, página não encontrada
+        </Text>
+        <Text style={[fonts.s, { color: colors.black }]}>
+          Por favor retorne a <Link style={[fonts.bold, { color: colors.green1, textDecorationLine: 'underline' }]} href='/'>página inicial</Link>
+        </Text>
+        <Link style={[fonts.bold, { color: colors.green1, textDecorationLine: 'underline' }]} href='/'>Voltar</Link>
+        <Text style={[fonts.xs, { color: colors.gray }]}>
+          Not Found - 404
+        </Text>
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+const styles = StyleSheet.create({});
