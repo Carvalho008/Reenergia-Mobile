abda3d95294bb670ff6ab00c74ba5e6a8c6b33d9
Lian Carvalho <carvalhocosta842@gmail.com>
2025-09-05 14:07:22 -0300

    Adiciona o display de DisplayEnergia na tela energias

--- reenergia/app/(tabs)/energias.tsx
+++ reenergia/app/(tabs)/energias.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 import { colors } from '@/constants/Colors';
 import { globalStyles } from '@/constants/Styles';
@@ -7,11 +7,20 @@ import { fonts } from '@/constants/Typografy';
 import Header from '@/components/ui/Header';
 
 export default function EnergiasScreen() {
+
+  const energiaExemplo = {
+    nome: 'Energia Solar',
+    imagem: 'https://png.pngtree.com/png-vector/20221128/ourmid/pngtree-icon-of-sun-png-image_6484830.png',
+    curiosidade: '#Top 2 mundial',
+    descricao: 'A energia solar vem se destacando pela a sua eficiência em locais de clima temperado, apesar do alto custo de instalação.',
+  }
+
   return (
     <View style={globalStyles.screenMain}>
       <Header icon='earth' title='Energias pelo mundo' />
       <View style={globalStyles.sectionBlock}>
         <Text style={[fonts.m, fonts.bold, { color: colors.green2 }]}>Destaque</Text>
+        <DisplayEnergia energia={energiaExemplo}/>
       </View>
       <View style={globalStyles.sectionBlock}>
         <Text style={[fonts.m, fonts.bold, { color: colors.green1 }]}>Gráficos</Text>
@@ -21,3 +30,69 @@ export default function EnergiasScreen() {
 }
 
 const styles = StyleSheet.create({});
+
+
+interface Energia {
+  nome: string;
+  curiosidade: string;
+  descricao: string;
+  imagem?: string;
+}
+
+type DisplayEnergiaProps = {
+  energia: Energia;
+};
+
+function DisplayEnergia({energia} : DisplayEnergiaProps) {
+  return(
+    <View style={displayStyles.displayContainer}>
+      <View style={displayStyles.displayImgContainer}>
+        <Image
+          source={{uri: energia.imagem}}
+          style={displayStyles.displayImg}
+          resizeMode='contain'
+        />
+      </View>
+      <View style={displayStyles.infosContainer}>
+        <View style={displayStyles.head}>
+          <Text style={[fonts.bold, fonts.m, {color: colors.black}]}>{energia.nome}</Text>
+          <Text style={[fonts.xs, {color: colors.gray}]}>{energia.curiosidade}</Text>
+        </View>
+          <Text style={[fonts.s, {color: colors.gray1}]}>{energia.descricao}</Text>
+      </View>
+    </View>
+  );
+}
+
+const displayStyles = StyleSheet.create({
+  displayContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+  },
+  displayImgContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    aspectRatio: 1,
+    maxWidth: 200,
+    position: 'relative',
+  },
+  displayImg: {
+    width: '100%',
+    height: '100%'
+  },
+  infosContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    gap: 8,
+  },
+  head: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 2,
+  }
+
+});
