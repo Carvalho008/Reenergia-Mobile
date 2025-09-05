6163b29dbbfe2859a162edfafa25ef78565913f2
Lian Carvalho <carvalhocosta842@gmail.com>
2025-09-05 18:13:06 -0300

    Botões da tela Home

--- reenergia/app/(tabs)/index.tsx
+++ reenergia/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, Pressable, StyleSheet } from 'react-native';
 
 import { colors } from '@/constants/Colors';
 import { globalStyles } from '@/constants/Styles';
@@ -16,6 +16,18 @@ export default function HomeScreen() {
         </Text>
         <Text style={[fonts.s, { color: colors.gray }]}>Compare fontes, entenda os impactos e
           tome decisões conscientes.</Text>
+
+        <View style={styles.buttonsContainer}>
+          <Pressable
+            style={({ pressed }) => [{ flex: 1 }, globalStyles.button, globalStyles.bPrimary, pressed && globalStyles.bPrimaryPress]}>
+            Comece agora
+          </Pressable>
+
+          <Pressable
+            style={({ pressed }) => [{ flex: 1 }, globalStyles.button, globalStyles.bSecondary, pressed && globalStyles.bSecondaryPress]}>
+            Saiba mais
+          </Pressable>
+        </View>
       </View>
       <View style={styles.introImgContainer}>
         <Image
@@ -39,5 +51,10 @@ const styles = StyleSheet.create({
   introImg: {
     width: '100%',
     height: '100%',
+  },
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 25,
   }
 });
